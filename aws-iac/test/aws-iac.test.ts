5f242024cc59ac2960f5a53b0fd930ce9cc4941d
dmtfix <DmitrikKuzmin@gmail.com>
2022-01-25 16:47:37 +0200

    aws-Cdk init, js

---
+++ aws-iac/test/aws-iac.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsIac from '../lib/aws-iac-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-iac-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsIac.AwsIacStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
