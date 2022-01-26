78bcaac4bd898fe731ee0e3152acbc20466942d0
dmtfix <DmitrikKuzmin@gmail.com>
2022-01-26 16:49:02 +0200

    set up s3, deploy, - cloudfront

--- aws-iac/lib/aws-iac-stack.ts
+++ aws-iac/lib/aws-iac-stack.ts
@@ -1,16 +1,23 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+//import { Stack, StackProps} from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import  * as s3deploy  from '@aws-cdk/aws-s3-deployment';
 
-export class AwsIacStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class AwsIacStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Set up a bucket
+    const websiteBucket = new s3.Bucket(this, 'GatsbyAlpBacket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html'  
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsIacQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset('../public')],
+      destinationBucket: websiteBucket,
+    });
+    
+
   }
 }
