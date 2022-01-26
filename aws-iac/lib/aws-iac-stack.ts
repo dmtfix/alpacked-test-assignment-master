d616c6b6e61374393deae1ac8a379ff6cc4f2aec
dmtfix <DmitrikKuzmin@gmail.com>
2022-01-26 17:31:43 +0200

    +cloudfront

--- aws-iac/lib/aws-iac-stack.ts
+++ aws-iac/lib/aws-iac-stack.ts
@@ -1,7 +1,8 @@
-//import { Stack, StackProps} from 'aws-cdk-lib';
 import * as cdk from '@aws-cdk/core';
 import * as s3 from '@aws-cdk/aws-s3';
-import  * as s3deploy  from '@aws-cdk/aws-s3-deployment';
+import * as s3deploy  from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
 
 export class AwsIacStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,11 +14,17 @@ export class AwsIacStack extends cdk.Stack {
       websiteIndexDocument: 'index.html'  
     });
 
+    // Loading up to bucket
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
       sources: [s3deploy.Source.asset('../public')],
       destinationBucket: websiteBucket,
     });
     
+    //
+    new cloudfront.Distribution(this, 'myDist', {
+      defaultBehavior: { origin: new origins.S3Origin(websiteBucket) },
+    });
+  
 
   }
 }
